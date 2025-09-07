0271d5daa48d5020db884d1e767116bd092f8d28
Travis Luong <travis@travisluong.com>
2025-09-06 20:38:35 -0700

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,25 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const buildScript = `source ~/.nvm/nvm.sh
+npm install
+cd tech-stack-studio-website
+npm install
+npm run build
+cd ..
+pm2 reload ecosystem.config.js`;
+
 module.exports = {
   apps: [
     {
       name: "tech-stack-studio-website",
       script: "node_modules/next/dist/bin/next",
+      args: "start",
+      cwd: "./tech-stack-studio-website",
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
     },
   ],
 
@@ -18,7 +32,7 @@ module.exports = {
       repo: process.env.GIT_REPOSITORY,
       path: process.env.DESTINATION_PATH,
       "pre-deploy-local": "",
-      "post-deploy": "./build.sh",
+      "post-deploy": buildScript,
       "pre-setup": "",
     },
   },
